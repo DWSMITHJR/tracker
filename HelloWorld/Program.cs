4a96fdd50f93e294d54b66a6d48b886944175956
Donald Smith <secure@donaldwsmithjr.com>
2025-11-11 14:42:30 -0500

    Clean up solution by removing unused files and services
    - Remove TestController, PortFinder, PortScanner, and HelloWorld sample
    - Remove unused AppSettingsService and IAppSettingsService
    - Remove Tracker.AuthTests project
    - Clean up MainLayout.razor

--- HelloWorld/Program.cs
+++
@@ -1,6 +0,0 @@
-// Simple test program to verify .NET installation
-Console.WriteLine("Hello, .NET is working!");
-Console.WriteLine($"Current directory: {Environment.CurrentDirectory}");
-Console.WriteLine($".NET Version: {Environment.Version}");
-Console.WriteLine("\nPress any key to exit...");
-Console.ReadKey();
